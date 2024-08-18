91b9cb28eb3477af191e21bb4eb9369e88c6f83e
Adhil Abubacker <adhilkv313@gmail.com>
2024-08-18 17:07:37 +0530

    first commit

--- alpha_backend/urls.py
+++ alpha_backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('opstmt.urls')),
     path('api/', include('assetnliabs.urls')),  
     path('api/', include('ocaocl.urls')),  
